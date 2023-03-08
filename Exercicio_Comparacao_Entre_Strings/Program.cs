1010844578ef7b4112ab1f492b603f1e5a8090f4
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-08 00:04:45 -0300

    Add project files.

---
+++ Exercicio_Comparacao_Entre_Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string val1;
+        string? val2;
+
+        Console.WriteLine("Digite a referencia: ");
+        val1=Console.ReadLine();
+
+        Console.WriteLine("Digite o que ira comparar: ");
+        val2 = Console.ReadLine();
+
+        if (val1.Equals(val2))
+        {
+            Console.WriteLine("Sao iguais");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao iguais");
+        }
+    }
+}
